a3f7bb3029fbbc4395d66de7e3b1851592428dc9
wangchao <wang153723482@gmail.com>
2018-03-30 20:02:21 +0800

    增加一个header

--- http-world-java/src/main/java/com/wangc/H4Header.java
+++ http-world-java/src/main/java/com/wangc/H4Header.java
@@ -2,6 +2,7 @@ package com.wangc;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
@@ -19,7 +20,7 @@ public class H4Header {
     public static void postHeader() throws IOException {
         CloseableHttpClient client = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet("http://127.0.0.1:8080/helloHeader");
-//        httpGet.setHeader("xx","hahhah");
+        httpGet.setHeader("xx","hahhah");
         httpGet.setHeader("content","text");
         CloseableHttpResponse response = client.execute(httpGet);
         System.out.println( response.getStatusLine().getStatusCode() );
